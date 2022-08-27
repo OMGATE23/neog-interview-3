d98f208c6ebbfc1e82a73748096e5ee91eb93c9e
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-08-27 17:52:49 +0530

    improved the neog interview 4 question

--- src/App.js
+++ src/App.js
@@ -3,38 +3,40 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState("")
+  const [Tag, setTag] = useState("div")
 
-  const [text, setText] = useState()
-  const [element, setElement] = useState(<div>{text}</div>)
   function changeHandler(event){
     let inputText = event.target.value
     setText(inputText)
-    setElement(<div>{inputText}</div>)
-    
   }
 
-  function changeElement(input){
-    switch (input){
-      case ("h1"):
-        return (setElement(<h1>{text}</h1>));
+  function changeTag(input){
+    switch(input){
+      case"h1":
+        setTag("h1");
         break;
-      case ("h2"):
-        return (setElement(<h2>{text}</h2>));
+      
+      case"h2":
+        setTag("h2");
         break;
 
-        case ("h3"):
-          return (setElement(<h3>{text}</h3>));
-          break;
+      case "h3":
+        setTag("h3");
+        break;
+
+
     }
+
   }
+
   return (
     <div className="App">
-      <input type = "text" onChange={(event)=>{changeHandler(event)}}></input>
-      <button onClick={()=> changeElement("h1")}>h1</button>
-      <button onClick={()=> changeElement("h2")}>h2</button>
-      <button onClick={()=> changeElement("h3")}>h3</button>
-      {element}
-
+      <input type="text" onChange = {(event) => {changeHandler(event)}}></input>
+      <Tag>{text}</Tag>
+      <button onClick = {()=> changeTag("h1")}>h1</button>
+      <button onClick = {()=> changeTag("h2")} >h2</button>
+      <button onClick = {()=> changeTag("h3")}>h3</button>
     </div>
   );
 }
